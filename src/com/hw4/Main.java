b0af69c11c96d22e0b703b787b1aa07cf196295b
ATMAS <atmasbooking@gmail.com>
2022-03-30 20:37:45 +0300

    01

--- src/com/hw4/Main.java
+++ src/com/hw4/Main.java
@@ -2,7 +2,19 @@ package com.hw4;
 
 public class Main {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args)
+    {
+////Задание1
+        int i = 0;
+        while (i !=10)
+        {
+            i++;
+            System.out.print(i + " ");
+        }
+        for (i=10;i>=1;i--)
+        {
+            System.out.print(i + " ");
+        }
+        //System.out.println(i);
     }
 }
